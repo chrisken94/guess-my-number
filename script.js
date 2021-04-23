53b8c7b2000105a3dd653697c7e77aece913fcb0
keken <chriskendry9@gmail.com>
2021-04-23 12:40:34 +0800

    update function (drier method)

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 "use strict";
 
 // defining the random number
-let secretNumber = Math.trunc(Math.random() * 30) + 1;
-// to put the random number in the html content
-let numberHtml = document.querySelector(".number");
-
-// defining the "user's score" to variable
-let scoreHtml = document.querySelector(".score");
-
-// defining the "highscore point" to variable
-let highScore = document.querySelector(".highscore");
-
-// defining the body selector for color
-let bodyColor = document.querySelector("body");
-
-// defining the "message" to variable
-let messageHtml = document.querySelector(".message"),
+let secretNumber = Math.trunc(Math.random() * 30) + 1,
+  // to put the random number in the html content
+  numberHtml = document.querySelector(".number"),
+  // defining the "user's score" to variable
+  scoreHtml = document.querySelector(".score"),
+  // defining the "highscore point" to variable
+  highScore = document.querySelector(".highscore"),
+  // defining the body selector for color
+  bodyColor = document.querySelector("body"),
+  // defining the "message" to variable
+  messageHtml = document.querySelector(".message"),
   // defining the "check button"
   checkButton = document.querySelector(".check"),
   // defining the "again button"
@@ -27,25 +23,30 @@ againButton.disabled = true;
 // to define the user's score in variable in our own code (not depend on DOM) (must be outside the function, if inside the function, the value will always be the same initial value every time the function run) (IMPORTANT LESSON : recommended practice to store data in our own code not depend on the DOM)
 let scoreInCode = 5;
 
+// a function to define the message (dry principle)
+const displayMessage = function (message) {
+  messageHtml.textContent = message;
+};
+
 const checkFunc = function () {
   // define the guess html to variable and convert it to Number format
   const guess = Number(document.querySelector(".guess").value);
 
   // when there is no guesses/input
   if (!guess) {
-    messageHtml.textContent = "No number!";
+    displayMessage("No Number!");
     // to disable the "again button"
     againButton.disabled = true;
   }
   // when player wins
   else if (guess === secretNumber) {
-    messageHtml.textContent = "🎉 Correct Number!";
+    displayMessage("🎉 Correct Number!");
     // to change the background color
     bodyColor.style.backgroundColor = "#60b347";
-    bodyColor.style.transition = "all 1s";
+    bodyColor.style.transition = "all 1.5s";
     // to make the square of the number wider
     numberHtml.style.width = "30rem";
-    numberHtml.style.transition = "all 1s";
+    numberHtml.style.transition = "all 1.5s";
     // to show the number
     numberHtml.textContent = secretNumber;
     // to disable the "check" button
@@ -60,8 +61,7 @@ const checkFunc = function () {
   }
   // when input guess is wrong (method #1 (dry principle))
   else if (guess !== secretNumber) {
-    messageHtml.textContent =
-      guess > secretNumber ? "⏫Too high!" : "⏬Too low!";
+    displayMessage(guess > secretNumber ? "⏫Too high!" : "⏬Too low!");
     scoreInCode--;
     scoreHtml.textContent = scoreInCode;
     // to disable the "again" button
@@ -70,7 +70,7 @@ const checkFunc = function () {
 
   // when player's score decreased to zero, telling them they lost the game and disabled the "check" button
   if (scoreInCode === 0) {
-    messageHtml.textContent = "🤯 You lost the game!";
+    displayMessage("🤯 You lost the game!");
     // to disable the button- preventing from infinite round
     checkButton.disabled = true;
     // to enable the "again button"
@@ -95,7 +95,7 @@ const againFunc = function () {
   // to restore the background color
   bodyColor.style.backgroundColor = "#222";
   // to restore the message back to "start guessing..." from "Correct Number"
-  messageHtml.textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   // to restore the guess value back to empty string
   let guess = document.querySelector(".guess");
   guess.value = "";
@@ -113,14 +113,6 @@ const againFunc = function () {
 // the global variable of "guess"
 let guess = document.querySelector(".guess");
 
-// number only on input guess
-const guessNumOnly = function (evt) {
-  let num = String.fromCharCode(evt.which);
-  if (!/[0-9]/.test(num)) {
-    evt.preventDefault();
-  }
-};
-
 // trigerring the "check" button with "enter" key in "input guess of user"
 guess.addEventListener("keyup", function (event) {
   if (event.keyCode === 13) {
@@ -133,6 +125,14 @@ guess.addEventListener("keyup", function (event) {
     againButton.click();
   }
 });
+
+// number only on input guess
+const guessNumOnly = function (evt) {
+  let num = String.fromCharCode(evt.which);
+  if (!/[0-9]/.test(num)) {
+    evt.preventDefault();
+  }
+};
 
 document.querySelector(".check").addEventListener("click", checkFunc);
 document.querySelector(".again").addEventListener("click", againFunc);
